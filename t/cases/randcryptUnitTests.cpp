c6653e92f2d33da696d6f7c99dc09f42167cb9c1
chickchirik <orangeboi@yandex.ru>
2020-04-03 17:07:02 +0300

    added example test case

---
+++ t/cases/randcryptUnitTests.cpp
@@ -0,0 +1,17 @@
+/*
+
+    randcryptUnitTests.cpp
+
+    Created by <chickchirik> on 09/10/2019.
+
+    DESCRIPTION:
+    Tests for randcrypt.
+
+*/
+
+#include "Catch2.hpp"
+#include "randcrypt.hpp"
+
+TEST_CASE("randcrypt tests", "[randcrypt]") {
+    REQUIRE(1 == 1);
+}
