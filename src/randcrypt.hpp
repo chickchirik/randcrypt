75c12b4571e8fe98f57058d77126b528c42ed4bd
chickchirik <orangeboi@yandex.ru>
2020-04-03 16:44:59 +0300

    added randcrypt.hpp header

---
+++ src/randcrypt.hpp
@@ -0,0 +1,11 @@
+/*
+
+    randcrypt.hpp
+
+    Created by <chickchirik> on 03/04/2019.
+
+    DESCRIPTION:
+    randcrypt is an ecryption/decryption software
+    that uses random algorithms.
+
+*/
