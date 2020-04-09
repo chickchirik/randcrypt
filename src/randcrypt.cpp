d58fee21917075028b3f760abaf069d3f9e5936c
chickchirik <orangeboi@yandex.ru>
2020-04-09 17:22:58 +0300

    added placeholders for functions definitions

---
+++ src/randcrypt.cpp
@@ -0,0 +1,19 @@
+/*
+
+    randcrypt.cpp
+
+    Created by <chickchirik> on 09/04/2019.
+
+    DESCRIPTION:
+    randcrypt is an ecryption/decryption software
+    that uses random algorithms.
+
+*/
+#include "randcrypt.hpp"
+
+namespace randcrypt {
+    char* encode(char* filepathIn, char* filepathOut) {}
+    char* encode(char* data) {}
+    void  decode(char* filepathIn, char* filepathOut, char* algoSeriesHash) {}
+    char* decode(char* algoSeriesHash) {}
+}
